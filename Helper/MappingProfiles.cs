a38a72d9b35ba124311af144c80575e6a84f26c4
coolb0y <57170752+coolb0y@users.noreply.github.com>
2024-03-27 16:27:17 +0530

    Get api for pokemon in c#

---
+++ Helper/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using webapi.Dto;
+using webapi.models;
+
+namespace webapi.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pokemon, PokemonDto>();
+        }
+    }
+}
